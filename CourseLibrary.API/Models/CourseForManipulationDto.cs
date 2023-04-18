ea9745c3dc201718457174fbe1f0318d8ac783f9
Samuel Kirigha <dkirigha18@gmail.com>
2023-04-18 06:15:12 +0300

    added attribute validation on clas level

--- CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public abstract class CourseForManipulationDto: IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public abstract class CourseForManipulationDto //: IValidatableObject
     {
         [Required(ErrorMessage = "You should fill out a title")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have  more than 100 characters.")]
@@ -11,13 +13,13 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500, ErrorMessage = "The Description should have more than 1500 characters")]
         public virtual string Description { get; set; }  = string.Empty;
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+       /* public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if(Title == Description)
             {
                 yield return new ValidationResult(
                     "The provided description should be different from title", new[] {"Course"});
             }
-        }
+        }*/
     }
 }
